8e173ba886c8d22405d544aede0af866da82e83c
aravindv <v_6arav@hotmail.com>
2024-05-10 09:25:44 +0530

    Modified the code to delete folders and post a message that all files were deleted successfully

--- personal_pythonProject/file_folder_delete/app.py
+++ personal_pythonProject/file_folder_delete/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
+import shutil
 
 app = Flask(__name__)
 folder_path = '/Users/aravindv/Downloads/to_be_deleted'  # Path to the directory
@@ -18,24 +19,11 @@ def index():
 @app.route('/delete', methods=['POST'])
 def delete_files():
     try:
-        remove_contents(folder_path)
-        return redirect(url_for('index'))
+        shutil.rmtree(folder_path)
+        os.makedirs(folder_path)
+        return render_template('index.html', files=[], message='All files in the folder have been deleted successfully!')
     except Exception as e:
         return str(e)
-
-
-def remove_contents(path):
-    for root, dirs, files in os.walk(path, topdown=False):
-        for name in files:
-            file_path = os.path.join(root, name)
-            if os.path.isfile(file_path):
-                os.remove(file_path)
-        for name in dirs:
-            dir_path = os.path.join(root, name)
-            if os.path.isdir(dir_path) and not os.path.islink(dir_path):
-                os.rmdir(dir_path)
-
-    return 'Contents of the directory deleted.'
 
 
 if __name__ == '__main__':
