ac5b7b13ef72ac6ea86b50ebfc161149c23bccc3
aravindv <v_6arav@hotmail.com>
2024-10-18 16:15:42 +0530

    alarm-clock code

---
+++ personal_pythonProject/python_power-ups/alarm_clock/alarm_clock.py
@@ -0,0 +1,24 @@
+
+
+import datetime
+import time
+from playsound import playsound
+
+
+def set_alarm(alarm_time):
+    """The program checks the time every 30 seconds until the current time matches the alarm time, at which point it
+    plays the sound and prints the message."""
+    print(f"Alarm set for {alarm_time}")
+    while True:
+        now = datetime.datetime.now()
+        current_time = now.strftime("%H:%M")
+        if current_time == alarm_time:
+            print("Wake up!")
+            playsound('path_to_sound.mp3')  # Make sure to have a sound file
+            break
+        time.sleep(30)  # Check every 30 seconds
+
+
+if __name__ == "__main__":
+    alarm_time = input("Enter the time for the alarm (HH:MM): ")
+    set_alarm(alarm_time)
